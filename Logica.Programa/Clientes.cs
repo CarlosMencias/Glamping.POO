6e8de7b40a460c87a7b351bc9ab9dcd3b0073c50
Carlos Mencias <menciasleal@gmail.com>
2023-07-17 23:12:14 -0500

    Agregar archivos de proyecto.

---
+++ Logica.Programa/Clientes.cs
@@ -0,0 +1,25 @@
+﻿namespace Logica.Programa
+{
+    public abstract class Clientes
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+
+        public string? Correo { get; set; }
+        public int Celular { get; set; }
+        public int Huespedes { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nombre.............:{Nombre}\n" +
+                   $"Apellido...........:{Apellido}" +
+                   $"Correo.............:{Correo}" +
+                   $"Celular............:{Celular}" +
+                   $"Huespedes..........:{Huespedes}";
+        }
+
+        public abstract decimal GetValueToPay();
+    }
+
+    
+}
---
+++ Logica.Programa/Precios.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Programa
+{
+    public class Precios : Clientes
+    {
+        public decimal _precio;
+
+        public Precios()
+        {
+            _precio = 5;
+        }
+        public decimal Precio 
+        { 
+            get => _precio; 
+            set => _precio = (value); 
+        }
+
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Huespedes * Precio;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                   $"Precio por persona......:{Precio,20}\n" +
+                   $"Total a pagar...........:{GetValueToPay(),20}\n";
+
+
+
+        }
+    }
+
+
+}
